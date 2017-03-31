adf7884f0beb0c0be8a372c3cd3c8cddf86f12b0
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2017-03-31 14:13:47 -0300

    Analytics

--- parts/scripts.php
+++ parts/scripts.php
@@ -53,6 +53,20 @@
 //        });
     });
 </script>
+
+<!-- Analytics -->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-96582868-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
+
 <!--[if lt IE 9]>
 <script src="assets/plugins/respond.js"></script>
 <script src="assets/plugins/html5shiv.js"></script>
