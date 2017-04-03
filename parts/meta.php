150807e1ee11335e29bc29b393bb39a533d6c128
joaohenriqueabreu <joaohenriqueabreu@gmail.com>
2017-04-03 16:55:19 -0300

    Metas

--- parts/meta.php
+++ parts/meta.php
@@ -1,26 +1,19 @@
+<title>Vende aí</title>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
+<meta name="description" content="A maneira mais rápida e fácil de se abrir um negócio virtual">
 <meta name="author" content="">
+<meta name="keywords" content="negocios online, negocios internet, ">
+<meta name="robots" content="index, follow">
 
-<meta itemprop="name" content="Vitrinet">
+<meta itemprop="name" content="Vende aí">
 <meta itemprop="description" content="A maneira mais rápida e fácil de se abrir um negócio virtual">
-<meta itemprop="image" content="http://www.selladd.com.br/assets/img/logo/logo-mini.png">
+<meta itemprop="image" content="http://www.vendeai.com/assets/img/logo/logo-mini.png">
 
-<meta property="og:title" content="Vitrinet"/>
+<meta property="og:title" content="Vende aí"/>
 <meta property="og:type" content="" />
-<meta property="og:url" content="wwww.selladd.com.br"/>
-<meta property="og:image" itemprop="image" content="http://www.selladd.com.br/assets/img/logo/logo-mini.png"/>
-<meta property="og:description" content="{{ $description }}"/>
-<meta property="og:site_name" content="@yield('og_category')"/>
-<meta property="og:price:amount" content="@yield('og_value')"/>
-<meta property="og:price:currency" content="R$"/>
-
-<meta property="og:title" content="@{{ ngMeta['og:title'] }}"/>
-<meta property="og:type" content="article"/>
-<meta property="og:url" content="@{{ ngMeta['og:url'] }}"/>
-<meta property="og:image" content="@{{ ngMeta['og:image'] }}"/>
-<meta property="og:description" content="@{{ ngMeta['og:description'] }}"/>
-<meta property="og:site_name" content="comOferta.com"/>
-<meta property="og:price:amount" content="@{{ ngMeta['og:price'] }}"/>
-<meta property="og:price:currency" content="R$"/>
+<meta property="og:url" content="wwww.vendeai.com"/>
+<meta property="og:image" itemprop="image" content="http://www.vendeai.com/assets/img/logo/logo-mini.png">
+<meta property="og:description" content="A maneira mais rápida e fácil de se abrir um negócio virtual"/>
+<meta property="og:site_name" content="Vende aí"/>
